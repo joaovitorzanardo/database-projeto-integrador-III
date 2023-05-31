fc4706e1b1c51759652005fbfebc5d7fb89854c2
João Vitor Zanardo <100529680+joaovitorzanardo@users.noreply.github.com>
2023-05-30 22:01:01 -0300

    Update criacao-tabelas.sql

--- criacao-tabelas.sql
+++ criacao-tabelas.sql
@@ -5,19 +5,13 @@ CREATE TABLE users (
   email VARCHAR(200) UNIQUE NOT NULL,
   password VARCHAR(60) NOT NULL,
   organization VARCHAR(200) NOT NULL,
-  user_role VARCHAR(50) NOT NULL
+  team_id INTEGER
 );
 
 CREATE TABLE team (
   team_id INTEGER PRIMARY KEY,
   name VARCHAR(200) NOT NULL,
   description VARCHAR(200)
-);
-
-CREATE TABLE team_member (
-  team_member_id INTEGER PRIMARY KEY, 
-  user_id INTEGER,
-  team_id INTEGER
 );
 
 CREATE TABLE address (
@@ -55,8 +49,12 @@ CREATE TABLE task (
   task_id INTEGER PRIMARY KEY,
   client_id INTEGER NOT NULL,
   deadline DATE,
-  team_member_id INTEGER,
-  task_status INTEGER NOT NULL
+  user_id INTEGER,
+  task_status INTEGER NOT NULL,
+  product_id INTEGER NOT NULL,
+  task_type_id INTEGER,
+  description TEXT,
+  price DECIMAL(8, 2)
 );
 
 CREATE TABLE task_type (
@@ -64,18 +62,8 @@ CREATE TABLE task_type (
   description VARCHAR(200) NOT NULL
 );
 
-CREATE TABLE task_item (
-  task_item_id INTEGER PRIMARY KEY,
-  task_id INTEGER NOT NULL,
-  product_id INTEGER NOT NULL,
-  task_type_id INTEGER,
-  description TEXT,
-  task_status INTEGER NOT NULL,
-  price DECIMAL(8, 2)
-);
 
-ALTER TABLE team_member ADD FOREIGN KEY (user_id) REFERENCES users(user_id);
-ALTER TABLE team_member ADD FOREIGN KEY (team_id) REFERENCES team(team_id);
+ALTER TABLE users ADD FOREIGN KEY (team_id) REFERENCES team(team_id);
 
 ALTER TABLE client ADD FOREIGN KEY (address_id) REFERENCES address(address_id);
 
@@ -83,11 +71,9 @@ ALTER TABLE product ADD FOREIGN KEY (client_id) REFERENCES client(client_id);
 ALTER TABLE product ADD FOREIGN KEY (product_type_id) REFERENCES product_type(product_type_id);
 
 ALTER TABLE task ADD FOREIGN KEY (client_id) REFERENCES client(client_id);
-ALTER TABLE task ADD FOREIGN KEY (team_member_id) REFERENCES team_member(team_member_id);
-
-ALTER TABLE task_item ADD FOREIGN KEY (task_id) REFERENCES task(task_id);
-ALTER TABLE task_item ADD FOREIGN KEY (product_id) REFERENCES product(product_id);
-ALTER TABLE task_item ADD FOREIGN KEY (task_type_id) REFERENCES task_type(task_type_id);
+ALTER TABLE task ADD FOREIGN KEY (user_id) REFERENCES users(user_id);
+ALTER TABLE task ADD FOREIGN KEY (product_id) REFERENCES product(product_id);
+ALTER TABLE task ADD FOREIGN KEY (task_type_id) REFERENCES task_type(task_type_id);
 
 CREATE SEQUENCE user_sequence INCREMENT 1;
 CREATE SEQUENCE address_sequence INCREMENT 1;
@@ -97,5 +83,3 @@ CREATE SEQUENCE product_sequence INCREMENT 1;
 CREATE SEQUENCE product_type_sequence INCREMENT 1;
 CREATE SEQUENCE task_sequence INCREMENT 1;
 CREATE SEQUENCE task_type_sequence INCREMENT 1;
-CREATE SEQUENCE task_item_sequence INCREMENT 1;
-CREATE SEQUENCE team_member_sequence INCREMENT 1;
